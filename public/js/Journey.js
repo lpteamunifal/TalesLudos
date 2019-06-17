522c0c554f07a75d62152be6ec413528fe2047f2
Filipe Ribeiro <filipe.parisoto@gmail.com>
2019-06-17 00:27:13 -0300

    Atualização total da branch master para laravel

---
+++ public/js/Journey.js
@@ -0,0 +1,75 @@
+class Journey {
+
+    constructor(name){
+        this.name = name;
+        this.sceneNumber = 0;
+        this.scenes = [];
+    }
+
+    get getName(){
+        return this.name;
+    }
+
+    set setName(name){
+        this.name = name;
+    }
+
+    get getSceneNumber(){
+        return this.sceneNumber;
+    }
+
+    set setSceneNumber(sceneNumber){
+        this.sceneNumber = sceneNumber;
+    }
+    
+    get getScenes(){
+        return this.scenes;
+    }
+
+    set setScenes(scene){
+        this.scenes = scenes;
+    }
+    
+    getSceneById(id)
+    {
+    	return this.scenes[id];
+    }
+
+    getNextSceneNumber(){
+        this.sceneNumber++;
+        return this.sceneNumber;
+    }
+    
+    addScene(scene){
+    	this.scenes.push(scene);
+    }
+
+    getSceneById(id){
+        for(var i = 0; i < this.scenes.length; i++){
+            if(this.scenes[i].getId == id){
+                return this.scenes[i];
+            }
+        }
+        return null;
+    }
+
+    getSceneByName(name){
+        for(var i = 0; i < this.scenes.length; i++){
+            if(this.scenes[i].getName == name){
+                return this.scenes[i];
+            }
+        }
+        return null;
+    }
+
+    deleteSceneByName(name){
+        for(var i = 0; i < this.scenes.length; i++){
+            if(this.scenes[i].getName == name){
+                this.scenes.splice(i, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
