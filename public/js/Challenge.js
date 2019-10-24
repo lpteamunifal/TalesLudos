0fcd6f07236434ea1a59d2e8d38ba8a3868fecfa
henriquesw <henriqueswisniewski@gmail.com>
2019-10-24 17:07:01 -0300

    merge

--- public/js/Challenge.js
+++ public/js/Challenge.js
@@ -5,7 +5,16 @@ class Challenge {
         this.name = name;
         //Elemento do menu de cenas lateral no html
         this.element = "";
+        this.plugin = "p0";
 		this.data = {}
+    }
+
+    get getName(){
+        return this.name;
+    }
+
+    set setName(name){
+        this.name = name;
     }
 	
 }
